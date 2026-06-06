3de1c1bc14bbcc1553246bb0b806ef55ce7ab3a1
徐正财 <282287001@qq.com>
2026-06-06 17:24:47 +0800

    fix: drop dangling ActiveSceneBar import in Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,6 @@ import { HouseLayout } from '@/components/HouseLayout/HouseLayout';
 import { DeviceModal } from '@/components/DeviceModal/DeviceModal';
 import { VoiceControl } from '@/components/VoiceControl/VoiceControl';
 import { SceneMode } from '@/components/SceneMode/SceneMode';
-import { ActiveSceneBar } from '@/components/SceneMode/ActiveSceneBar';
 import { EnergyStats } from '@/components/EnergyStats/EnergyStats';
 import { DeviceLog } from '@/components/DeviceLog/DeviceLog';
 import { ToastContainer } from '@/components/Toast/ToastContainer';
